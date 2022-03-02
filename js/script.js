70ba9f5b4b8aedf025b37df2ee42090d4f44ef91
Zerkia <lordzerkia@gmail.com>
2022-03-02 13:07:44 +0100

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
 const url = "http://worldtimeapi.org/api/timezone/";
 
 const dropdown = document.getElementById("dropdown");
+const time = document.getElementById("time");
 dropdown.addEventListener("change", () => {
   console.log(dropdown.value);
   worldtimeapiFetch(url + dropdown.value);
+  clearInterval(timeloop);
+  const timeloop = setInterval(worldtimeapiFetch, 1000, url + dropdown.value);
+  console.log(time.value);
 });
 
 const worldtimeapiFetch = function (url) {
+
   fetch(url)
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
-      /*{
-  "abbreviation": "CET",
-  "client_ip": "87.59.14.119",
-  "datetime": "2022-02-17T21:01:50.952225+01:00", <----
-  "day_of_week": 4,
-  "day_of_year": 48,
-  "dst": false,
-  "dst_from": null,
-  "dst_offset": 0,
-  "dst_until": null,
-  "raw_offset": 3600,
-  "timezone": "Europe/Copenhagen", <----
-  "unixtime": 1645128110,
-  "utc_datetime": "2022-02-17T20:01:50.952225+00:00",
-  "utc_offset": "+01:00", <----
-  "week_number": 7
-        }*/
-
-      /*TODO: lav passende attributter ud fra de markerede JSON værdier som kan manipulerer med klokken  */
-      /* Altså vælg tidszone fra drop down, og set uret.
-    });
+      console.log(res.datetime);
+      time.innerHTML = res.datetime.substring(11, 19);
+  });
 };
+
+/*TODO: lav passende attributter ud fra de markerede JSON værdier som kan manipulerer med klokken  */
+/* Altså vælg tidszone fra drop down, og set uret. */
